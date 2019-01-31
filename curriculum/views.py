f895ef1e4d2a83cf10fe5cb752821c8aedf2bfd9
liviulimisca <liviu.limisca@gmail.com>
2019-01-31 19:43:01 +0200

    pip

--- curriculum/views.py
+++ curriculum/views.py
@@ -4,7 +4,7 @@ from curriculum.models import Experience
 def resume(request):
     all_experiences = Experience.objects.order_by("-id")
     context = {
-    'all experiences': all_experiences,
+    'all_experiences': all_experiences,
     }
 
     return render(request, 'base.html', context)
